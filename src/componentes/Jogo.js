5d2ed591c11da1111f4e9b04c610884b7e535e2d
William <wiliancosta51@hotmail.com>
2023-02-23 17:41:03 -0300

    gerando palavras aleatorias

--- src/componentes/Jogo.js
+++ src/componentes/Jogo.js
@@ -1,29 +1,27 @@
+import { useState } from "react";
 import palavras from "../palavras"
 
 
 
 export default function Jogo(){
 
+    const [mostrarPalavra, setMostrarPalavra] = useState("_ _ _ _ _ _");
 
+    const novaLista = [...palavras];
+    
     return (
-        <div className="botao" onClick={sortearPalavra}><span>Escolha Palavra</span></div>
+        <><div className="botao" onClick={SortearPalavra}><span>Escolher Palavra</span></div><div className="palavra">
+            <span className="palavraSorteada">{mostrarPalavra}</span>
+        </div></>
     )
     
-}
+    function SortearPalavra(){
 
-function sortearPalavra(){
+        const palavra = Math.floor(Math.random() * novaLista.length);
 
-    const novaLista = [...palavras]
+        const mostrando = novaLista[palavra];
 
-    const palavra = Math.floor(Math.random() * novaLista.length);
-
+        setMostrarPalavra(mostrando)
     
-
-    return (
-        <div>
-            <span>{palavra}</span>
-        </div>
-    )
-
-
+    }
 }
